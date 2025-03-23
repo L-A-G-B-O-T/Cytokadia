a77ca5fde73ded61a783fa302354afa160b00cfd
Alexander Yu <156768800+L-A-G-B-O-T@users.noreply.github.com>
2025-03-23 00:21:50 +0000

    chore: better physcs, show up on page! Verlet Integration

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,7 @@ const canvas = document.getElementById("mainCanvas");
 //This game will try to fit whatever window, so I will need to center my ctx drawings accordingly
 canvas.height = 620;
 canvas.width = 620 * (window.innerWidth - 20) / (window.innerHeight - 20);
+const canvasScaleFactor = canvas.height/(window.innerHeight - 20);
 
 const ctx = canvas.getContext("2d");
 
@@ -25,15 +26,49 @@ ctx.fillRegularPolygon(canvas.width / 2, canvas.height / 2, 100, 0, 3);
 class Node {
 	constructor(){
 		this.pos = new Vector(0, 0); //will be in pixels
-		this.vel = new Vector(0, 0); //pixel/millisecond; ignore maybe due to small enviroment?
+		this.posPrev = new Vector(0, 0); //stores the previous position for Verlet Integration
 		this.force = new Vector(0, 0);//mcg * pixel/millisecond^2
 		this.mass = 1;//mcg
 	}
 	physicsTick(deltaTime){ //deltaTime will be in milliseconds
 		this.force.set(0, 0);
-		//this.force.addSelf()
-		this.vel.addSelf(this.force.mulScalar(deltaTime/this.mass));
-		this.pos.addSelf(this.vel.mulScalar(deltaTime));
+		//add all forces onto it
+
+		const vel = this.pos.sub(this.posPrev);
+		vel.addSelf(this.force.divScalar(this.mass));
+		
+		vel.mulScalarSelf(0.95) // damping factor;
+
+		const posNext = this.pos.add(vel);
+		this.posPrev.copy(this.pos);
+		this.pos.copy(posNext);
+		//this.pos next = this.pos + (this.pos - this.posPrev + this.force/this.mass)
+
+		this.stayInBound(0, 0, canvas.width, canvas.height); //optional
+		
+
+
+	}
+	stayInBound(minX, minY, maxX, maxY){
+		const posArr = this.pos.toArray();
+		const vel = this.pos.sub(this.posPrev);
+		if (posArr[0] < minX){ //reverse X velocity in Verlet
+			vel.copyX(vel.invert());
+			this.pos.set(minX, undefined);
+			this.posPrev.copy(this.pos.sub(vel));
+		} if (posArr[1] < minY){
+			vel.copyY(vel.invert());
+			this.pos.set(undefined, minY);
+			this.posPrev.copy(this.pos.sub(vel));
+		} if (posArr[0] > maxX){
+			vel.copyX(vel.invert());
+			this.pos.set(maxX, undefined);
+			this.posPrev.copy(this.pos.sub(vel));
+		} if (posArr[1] > maxY){
+			vel.copyY(vel.invert());
+			this.pos.set(undefined, maxY);
+			this.posPrev.copy(this.pos.sub(vel));
+		}
 	}
 }
 
@@ -45,7 +80,7 @@ class SpringEdge {
 		this.damping = 0;
 	}
 	calcForce(){ //returns amount of pulling force of the spring. Pushing force is negative. 
-		if (typeof this.A != Node || typeof this.B != Node){
+		if (!(this.A instanceof Node && this.B instanceof Node)){
             throw TypeError("A or B of <SpringEdge> object is not of type <Node>");
         }
         const currentLength = this.A.pos.sub(this.B.pos).length();
@@ -58,14 +93,68 @@ class DistanceConstraint {
 		this.A = this.B = null;
 		this.distance = 100; //default
 	}
-    constrain(){ //sets the position of this.B so it lies in the distance constraint
-        if (typeof this.A != Node || typeof this.B != Node){
+    constrain(){ //sets the position of nodes so they lie in the distance constraint
+        if (!(this.A instanceof Node && this.B instanceof Node)){
             throw TypeError("A or B of <SpringEdge> object is not of type <Node>");
         }
-        const disp = this.B.pos.sub(this.A.pos);
-        disp.normalizeSelf().mulScalarSelf(this.distance);
-        this.B.pos.copy(this.A.pos.add(disp));
+		const midPos = this.A.pos.add(this.B.pos).divScalarSelf(2);
+		const AToMidDir = midPos.sub(this.A.pos).normalizeSelf();
+		if (AToMidDir.length() == 0){
+			console.log("catch clip");
+			AToMidDir.set(1, 0);
+		}
+		this.B.pos.copy(midPos.add(AToMidDir.mulScalar(this.distance/2)));
+		this.A.pos.copy(midPos.add(AToMidDir.mulScalar(-this.distance/2)));
     }
+}
+
+class AngleConstraint {
+	constructor(){
+		this.A = this.B = this.C = null;
+		this.angle = Math.PI / 2;
+	}
+	constrain(){ //sets the position of 
+
+	}
+}
+
+class HardWormBody { //DistanceConstraint(Node, Node) in a line
+	constructor(nodeCount, internodeLength, headPos){
+		if (!(headPos instanceof Vector))
+			throw TypeError(`headPos of HardWormBody should be <Vector>, not <${headPos.constructor.name}>`);
+		this.nodes = [];
+		this.edges = [];
+		for (let i = 0; i < nodeCount; i++){
+			const newNode = new Node();
+			newNode.pos.copy(headPos);
+			this.nodes.push(newNode);
+		}
+		for (let i = 1; i < nodeCount; i++){
+			const newEdge = new DistanceConstraint();
+			newEdge.A = this.nodes[i - 1];
+			newEdge.B = this.nodes[i];
+			newEdge.distance = internodeLength;
+			this.edges.push(newEdge);
+		}
+	}
+	tick(deltaTime){
+		for (const edge of this.edges){
+			edge.constrain();
+		}
+		for (const node of this.nodes){
+			node.physicsTick(deltaTime);
+		}
+		if (mouse.pressLeft)
+			this.nodes[0].pos.copy(mouse.pos);
+	}
+}
+
+class SoftWormBody { //SpringEdge(Node, Node) in a line
+
+}
+
+class PressureSoftBody { //SpringEdge(Node, Node) in a loop
+
 }
 
 class Cell { //pressure soft body
@@ -74,28 +163,53 @@ class Cell { //pressure soft body
 
 class Bacterium { //
 	constructor(){
-		
+		this.body = new HardWormBody(10, 20, new Vector(100, 100));
 	}
 	player(){
 		
 	}
-	tick(){
-		
+	tick(deltaTime){
+		this.body.tick(deltaTime);
 	}
 	draw(){
-		
+		for (const node of this.body.nodes){
+			ctx.beginPath();
+			const posArr = node.pos.toArray();
+			ctx.ellipse(posArr[0], posArr[1], 3, 3, 0, 0, Math.PI * 2);
+			ctx.strokeStyle = "white";
+			ctx.stroke();
+			ctx.closePath();
+		}
 	}
 }
 var bacteria = [];
+var deltaTime = 50;
+var mouse = {
+	pos : new Vector(0, 0),
+	pressLeft : false, 
+};
+
+canvas.onmousemove = function(e){
+	mouse.pos.set(e.offsetX * canvasScaleFactor, e.offsetY * canvasScaleFactor);
+};
+canvas.onmousedown = function(e){
+	mouse.pressLeft = true;
+};
+canvas.onmouseup = function(e){
+	mouse.pressLeft = false;
+};
 
 function mainloop(){
-	for (const bacterium of bacteria) bacterium.tick();
+	for (const bacterium of bacteria) bacterium.tick(deltaTime);
+
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (const bacterium of bacteria) bacterium.draw();
 }
 
 function initialize(){
+	console.log("init");
     bacteria.push(new Bacterium());
-	setInterval(mainloop, 50);
+	setInterval(mainloop, deltaTime);
 }
 
 var running = false;
